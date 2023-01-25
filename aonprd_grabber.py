49b787ab60e22b6b28427e218773a18e0289dd55
ankatern <ankatern@gmail.com>
2023-01-25 23:34:30 +0600

    load every creature by increment

--- aonprd_grabber.py
+++ aonprd_grabber.py
@@ -1,70 +1,86 @@
+import os
+import socket
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
 
-class Creature:
-    def __init__(self, url: str, name: str):
-        self.url = url
-        self.name = name
-
-
+# TODO add normal logging
 class AonprdGrabber:
     __url = "https://2e.aonprd.com/"
     __get = "AspxAutoDetectCookieSupport=1"
+    __uri_monster = "Monsters.aspx"
+    __uri_npc = "NPCs.aspx"
+    __curr_id = 1
+    __path_base = 'creatures'
+    __fail_counter = 0
+
+    MAX_FAILS = 500
+    MAX_REQUEST_ATTEMPTS = 10
+    TIMEOUT = 5
+    TIMEOUT_STREAM = 20
+    SLEEP_TIME = 5
 
     def __init__(self) -> None:
+        if not os.path.exists(self.__path_base):
+            try:
+                os.mkdir(self.__path_base)
+            except FileExistsError as exc:
+                print(exc)
         self.__session = requests.Session()
 
     def __get_html(self, url: str):
+        # TODO add time limit for requests
         print(url)
         url = url.replace('\\', '/')
         if url.find('?') > 0:
             fin_url = f'{url}&{self.__get}'
         else:
             fin_url = f'{url}?{self.__get}'
-        response = requests.get(fin_url)
+
+        response = self.__request_get(fin_url)
+        if response is None or response.status_code != 200:
+            return None
+
         html = response.content
         if response.apparent_encoding is None:
             print('broken encoding')
 
         return html
 
-    def get_creatures(self):
-        html = self.__get_html(f'{self.__url}Creatures.aspx')
+    def save_creature(self):
+        html = self.__get_creature_page()
+        self.__curr_id += 1
+        if html is None:
+            print(f'Can`t find creatue №{self.__curr_id - 1}')
+            self.__fail_counter += 1
+            if self.__fail_counter >= self.MAX_FAILS:
+                return None
+            else:
+                return True
+
         soup = BeautifulSoup(html, 'html.parser')
+        main = soup.find('div', id='main')
+        title = main.find('h1', {'class': 'title'})
+        if title is None:
+            print(f'Can`t find name for creatue №{self.__curr_id - 1}')
+            return None
 
-        table = soup.find('table', class_="rgMasterTable")
-        rows = table.find('tbody').find_all('tr')
-        letters = {}
-
-        for row in rows:
-            link = row.find('td').find('a')
-            url = link.get('href')
-            name = self.__format_name(link.text)
-            first_letter = name[0].upper()
-
-            if first_letter not in letters.keys():
-                letters[first_letter] = []
-
-            letters[first_letter].append(Creature(url, name))
-
-        return letters
-
-    def __format_name(self, name: str):
-        return name.lower().replace(' ', '_')
-
-    def save_creature(self, path: str, creature: Creature):
-        creature_url = f'{self.__url}{creature.url}'
-        html = self.__get_html(creature_url)
-        soup = BeautifulSoup(html, 'html.parser')
-        img = soup.find('img', class_='thumbnail')
+        creature_name = title.text
+        img = soup.find('img', {'class': 'thumbnail'})
         if img is not None:
-            print(f'Image has found for {creature.name}')
+            print(f'Image has found for {creature_name}')
+            dir_name = creature_name[0].upper()
+            path = self.__make_path(dir_name)
+
             src = img.get('src')
             img_url = f'{self.__url}{src}'
 
-            with open(f'{path}/{creature.name}.jpg', 'wb') as handler:
-                response = requests.get(img_url, stream=True)
+            print('start downloading img')
+            with open(f'{path}/{creature_name}.jpg', 'wb') as handler:
+                # response = requests.get(img_url, stream=True)
+                response = self.__request_get(img_url, True)
 
                 if not response.ok:
                     print(response)
@@ -74,5 +90,58 @@ class AonprdGrabber:
                         break
 
                     handler.write(block)
+            print('stop downloading img')
         else:
-            print(f'Image has not found for {creature.name}')
+            print(f'Image has not found for {creature_name}')
+
+        return True
+
+    def __get_creature_page(self):
+        id_str = f'?ID={self.__curr_id}'
+        creature_url = f'{self.__url}{self.__uri_monster}{id_str}'
+        html = self.__get_html(creature_url)
+        if html is None:
+            npc_url = f'{self.__url}{self.__uri_npc}{id_str}'
+            html = self.__get_html(npc_url)
+
+        return html
+
+    def __make_path(self, dir_name) -> str:
+        path = f'{self.__path_base}/{dir_name}'
+        if not os.path.exists(path):
+            try:
+                os.mkdir(path)
+            except FileExistsError as exc:
+                print(exc)
+
+        return path
+
+    def __request_get(self, fin_url, stream=False):
+        request_attempts = 0
+        response = None
+        while request_attempts < self.MAX_REQUEST_ATTEMPTS:
+            try:
+                if stream:
+                    response = requests.get(fin_url, timeout=self.TIMEOUT_STREAM, stream=True)
+                else:
+                    response = requests.get(fin_url, timeout=self.TIMEOUT)
+                break
+            except requests.Timeout as e:
+                print(e.strerror)
+                request_attempts += 1
+                print(request_attempts)
+                time.sleep(self.SLEEP_TIME)
+
+            except socket.timeout as e:
+                print(e.strerror)
+                request_attempts += 1
+                print(request_attempts)
+                time.sleep(self.SLEEP_TIME)
+
+            except ConnectionError as e:
+                print(e.strerror)
+                request_attempts += 1
+                print(request_attempts)
+                time.sleep(self.SLEEP_TIME)
+
+        return response
--- main.py
+++ main.py
@@ -4,22 +4,7 @@ from aonprd_grabber import AonprdGrabber
 
 if __name__ == '__main__':
     grabber = AonprdGrabber()
-    letters = grabber.get_creatures()
-    try:
-        os.mkdir('creatures')
-    except FileExistsError as exc:
-        print(exc)
 
-    for letter in letters:
-        data = letters[letter]
-        path = f'creatures/{letter}'
-        print(f'Start parsing creatures for letter {letter}. '
-              f'There are {len(data)} creatures')
-
-        try:
-            os.mkdir(path)
-        except FileExistsError as exc:
-            print(exc)
-
-        for creature in data:
-            grabber.save_creature(path, creature)
+    while True:
+        if grabber.save_creature() is None:
+            break
